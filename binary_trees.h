fd79ab61028cd1a4c3745f61da3efec6af012a9c
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-11-30 10:36:45 +0000

    depth of a node in a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -36,6 +36,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t max(size_t a, size_t b);
 size_t height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 
 #endif
